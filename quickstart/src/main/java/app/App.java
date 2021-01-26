3b02d288236ea7efdbd6fef62e49d575e2838a8c
hongli750210 <v-hongli1@microsoft.com>
2021-01-26 15:30:52 +0800

    Replace the REST API with Java SDK calls for Task 297 202101261528

--- quickstart/src/main/java/app/App.java
+++ quickstart/src/main/java/app/App.java
@@ -1,83 +1,223 @@
 package main.java.app;
 
-import main.java.service.SearchServiceClient;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.Context;
+import com.azure.search.documents.SearchClient;
+import com.azure.search.documents.SearchServiceVersion;
+import com.azure.search.documents.indexes.SearchIndexClient;
+import com.azure.search.documents.indexes.SearchIndexClientBuilder;
+import com.azure.search.documents.indexes.models.LexicalAnalyzerName;
+import com.azure.search.documents.indexes.models.SearchField;
+import com.azure.search.documents.indexes.models.SearchFieldDataType;
+import com.azure.search.documents.indexes.models.SearchIndex;
+import com.azure.search.documents.models.SearchOptions;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.IOException;
-import java.util.Properties;
+import java.io.InputStream;
+import java.time.Instant;
+import java.util.*;
 
 public class App {
 
     private static Properties loadPropertiesFromResource(String resourcePath) throws IOException {
-        var inputStream = App.class.getResourceAsStream(resourcePath);
+        InputStream inputStream = App.class.getResourceAsStream(resourcePath);
         var configProperties = new Properties();
         configProperties.load(inputStream);
         return configProperties;
     }
 
+    private static JSONObject loadJsonObjectFromResource(String resourcePath) {
+        InputStream inputStream = App.class.getResourceAsStream(resourcePath);
+        return new JSONObject(new JSONTokener(inputStream));
+    }
+
+    private static List<Map<String, Object>> createDocumentMap(JSONArray dcoJson) {
+        List<Map<String, Object>> documentMapList = new ArrayList<>();
+        for (int i = 0; i < dcoJson.length(); i++) {
+            JSONObject docObject = dcoJson.getJSONObject(i);
+            Map<String, Object> docMap = new HashMap<>();
+            documentMapList.add(docMap);
+            for (Iterator<String> iterator = docObject.keys(); iterator.hasNext(); ) {
+                String strKey = iterator.next();
+                switch (strKey) {
+                    case "@search.action":
+                    case "HotelId":
+                    case "HotelName":
+                    case "Description":
+                    case "Description_fr":
+                    case "Category":
+                        docMap.put(strKey, docObject.getString(strKey));
+                        break;
+                    case "Tags":
+                        List<String> tagList = new ArrayList<>();
+                        docMap.put(strKey, tagList);
+                        JSONArray tagsArray = docObject.getJSONArray(strKey);
+                        for (int j = 0; j < tagsArray.length(); j++) {
+                            tagList.add(tagsArray.getString(j));
+                        }
+                        break;
+                    case "ParkingIncluded":
+                        docMap.put(strKey, docObject.getBoolean(strKey));
+                        break;
+                    case "LastRenovationDate":
+                        docMap.put(strKey, Instant.parse(docObject.getString(strKey)));
+                        break;
+                    case "Rating":
+                        docMap.put(strKey, docObject.getDouble(strKey));
+                        break;
+                    case "Address":
+                        docMap.put(strKey, docObject.getJSONObject(strKey).toMap());
+                }
+            }
+        }
+        return documentMapList;
+    }
+
+    private static List<SearchField> createIndexFields(JSONArray fieldsJson) {
+        List<SearchField> searchFieldList = new ArrayList<>();
+        for (int i = 0; i < fieldsJson.length(); i++) {
+            JSONObject fieldObject = fieldsJson.getJSONObject(i);
+            SearchField searchField = new SearchField(fieldObject.getString("name"), SearchFieldDataType.fromString(fieldObject.getString("type")));
+            for (Iterator<String> iterator = fieldObject.keys(); iterator.hasNext(); ) {
+                String strKey = iterator.next();
+                switch (strKey) {
+                    case "key":
+                        searchField.setKey(fieldObject.getBoolean(strKey));
+                        break;
+                    case "filterable":
+                        searchField.setFilterable(fieldObject.getBoolean(strKey));
+                        break;
+                    case "searchable":
+                        searchField.setSearchable(fieldObject.getBoolean(strKey));
+                        break;
+                    case "sortable":
+                        searchField.setSortable(fieldObject.getBoolean(strKey));
+                        break;
+                    case "facetable":
+                        searchField.setFacetable(fieldObject.getBoolean(strKey));
+                        break;
+                    case "analyzer":
+                        searchField.setAnalyzerName(LexicalAnalyzerName.fromString(fieldObject.getString(strKey)));
+                        break;
+                    case "fields":
+                        searchField.setFields(createIndexFields(fieldObject.getJSONArray(strKey)));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            searchFieldList.add(searchField);
+        }
+        return searchFieldList;
+    }
+
     public static void main(String[] args) {
         try {
-            var config = loadPropertiesFromResource("/app/config.properties");
-            var client = new SearchServiceClient(
-                    config.getProperty("SearchServiceName"),
-                    config.getProperty("SearchServiceAdminKey"),
-                    config.getProperty("SearchServiceQueryKey"),
-                    config.getProperty("ApiVersion"),
-                    config.getProperty("IndexName")
-            );
+            Properties config = loadPropertiesFromResource("/app/config.properties");
+            SearchIndexClient indexClient = new SearchIndexClientBuilder()
+                    .endpoint(String.format(config.getProperty("SearchServiceEndPoint"),config.getProperty("SearchServiceName")))
+                    .credential(new AzureKeyCredential(config.getProperty("SearchServiceAdminKey")))
+                    .serviceVersion(SearchServiceVersion.getLatest())
+                    .buildClient();
+
 
 
 //Uncomment the next 3 lines in the 1 - Create Index section of the quickstart
-            if(client.indexExists()){ client.deleteIndex();}
-            client.createIndex("/service/index.json");
+            JSONObject indexJson = loadJsonObjectFromResource("/service/index.json");
+            SearchIndex searchIndex = new SearchIndex(indexJson.getString("name"));
+            JSONArray fields = indexJson.getJSONArray("fields");
+            searchIndex.setFields(createIndexFields(fields));
+            indexClient.createOrUpdateIndex(searchIndex);
+            SearchClient client = indexClient.getSearchClient(searchIndex.getName());
             Thread.sleep(1000L); // wait a second to create the index
 
 //Uncomment the next 2 lines in the 2 - Load Documents section of the quickstart
-            client.uploadDocuments("/service/hotels.json");
+            JSONObject documentJson = loadJsonObjectFromResource("/service/hotels.json");
+            client.mergeOrUploadDocuments(createDocumentMap(documentJson.getJSONArray("value")));
             Thread.sleep(2000L); // wait 2 seconds for data to upload
 
 //Uncomment the following 5 search queries in the 3 - Search an index section of the quickstart
             // Query 1
-            client.logMessage("\n*QUERY 1****************************************************************");
-            client.logMessage("Search for: Atlanta'");
-            client.logMessage("Return: All fields'");
-            client.searchPlus("Atlanta");
+            System.out.println("\n*QUERY 1****************************************************************");
+            System.out.println("Search for: Atlanta");
+            System.out.println("Return: All fields");
+            client.search("Atlanta").forEach(searchResult -> {
+                System.out.println("QUERY 1 search result*************************************************");
+                Map<String,Object> resultMap = searchResult.getDocument(Map.class);
+                for ( Iterator<String> iterator = resultMap.keySet().iterator(); iterator.hasNext(); ) {
+                    String key = iterator.next();
+                    System.out.println(String.format("%s : %s", key , resultMap.get(key)));
+                }
+            });
 
             // Query 2
-            client.logMessage("\n*QUERY 2****************************************************************");
-            client.logMessage("Search for: Atlanta");
-            client.logMessage("Return: HotelName, Tags, Address");
-            SearchServiceClient.SearchOptions options2 = client.createSearchOptions();
-            options2.select = "HotelName,Tags,Address";
-            client.searchPlus("Atlanta", options2);
+            System.out.println("\n*QUERY 2****************************************************************");
+            System.out.println("Search for: Atlanta");
+            System.out.println("Return: HotelName, Tags, Address");
+            SearchOptions searchOptions = new SearchOptions();
+            searchOptions.setSelect("HotelName", "Tags", "Address");
+            client.search("Atlanta", searchOptions, new Context("key1","value1")).forEach(searchResult -> {
+                System.out.println("QUERY 2 search result*************************************************");
+                Map<String,Object> resultMap = searchResult.getDocument(Map.class);
+                for ( Iterator<String> iterator = resultMap.keySet().iterator(); iterator.hasNext(); ) {
+                    String key = iterator.next();
+                    System.out.println(String.format("%s : %s", key , resultMap.get(key)));
+                }
+            });
 
             //Query 3
-            client.logMessage("\n*QUERY 3****************************************************************");
-            client.logMessage("Search for: wifi & restaurant");
-            client.logMessage("Return: HotelName, Description, Tags");
-            SearchServiceClient.SearchOptions options3 = client.createSearchOptions();
-            options3.select = "HotelName,Description,Tags";
-            client.searchPlus("wifi,restaurant", options3);
+            System.out.println("\n*QUERY 3****************************************************************");
+            System.out.println("Search for: wifi & restaurant");
+            System.out.println("Return: HotelName, Description, Tags");
+            searchOptions = new SearchOptions();
+            searchOptions.setSelect("HotelName", "Description", "Tags");
+            client.search("wifi,restaurant", searchOptions, new Context("key1","value1")).forEach(searchResult -> {
+                System.out.println("QUERY 3 search result*************************************************");
+                Map<String,Object> resultMap = searchResult.getDocument(Map.class);
+                for ( Iterator<String> iterator = resultMap.keySet().iterator(); iterator.hasNext(); ) {
+                    String key = iterator.next();
+                    System.out.println(String.format("%s : %s", key , resultMap.get(key)));
+                }
+            });
 
             // Query 4 -filtered query
-            client.logMessage("\n*QUERY 4****************************************************************");
-            client.logMessage("Search for: all");
-            client.logMessage("Filter: Ratings greater than 4");
-            client.logMessage("Return: HotelName, Rating");
-            SearchServiceClient.SearchOptions options4 = client.createSearchOptions();
-            options4.filter="Rating%20gt%204";
-            options4.select = "HotelName,Rating";
-            client.searchPlus("*",options4);
+            System.out.println("\n*QUERY 4****************************************************************");
+            System.out.println("Search for: all");
+            System.out.println("Filter: Ratings greater than 4");
+            System.out.println("Return: HotelName, Rating");
+            searchOptions = new SearchOptions();
+            searchOptions.setSelect("HotelName", "Rating");
+            searchOptions.setFilter("Rating gt 4");
+            client.search("*",searchOptions, new Context("key1","value1")).forEach(searchResult -> {
+                System.out.println("QUERY 4 search result*************************************************");
+                Map<String,Object> resultMap = searchResult.getDocument(Map.class);
+                for ( Iterator<String> iterator = resultMap.keySet().iterator(); iterator.hasNext(); ) {
+                    String key = iterator.next();
+                    System.out.println(String.format("%s : %s", key , resultMap.get(key)));
+                }
+            });
 
             // Query 5 - top 2 results, ordered by
-            client.logMessage("\n*QUERY 5****************************************************************");
-            client.logMessage("Search for: boutique");
-            client.logMessage("Get: Top 2 results");
-            client.logMessage("Order by: Rating in descending order");
-            client.logMessage("Return: HotelId, HotelName, Category, Rating");
-            SearchServiceClient.SearchOptions options5 = client.createSearchOptions();
-            options5.top=2;
-            options5.orderby = "Rating%20desc";
-            options5.select = "HotelId,HotelName,Category,Rating";
-            client.searchPlus("boutique", options5);
+            System.out.println("\n*QUERY 5****************************************************************");
+            System.out.println("Search for: boutique");
+            System.out.println("Get: Top 2 results");
+            System.out.println("Order by: Rating in descending order");
+            System.out.println("Return: HotelId, HotelName, Category, Rating");
+            searchOptions = new SearchOptions();
+            searchOptions.setSelect("HotelId", "HotelName", "Category", "Rating");
+            searchOptions.setOrderBy("Rating desc");
+            searchOptions.setTop(2);
+            client.search("boutique", searchOptions, new Context("key1","value1")).forEach(searchResult -> {
+                System.out.println("QUERY 5 search result*************************************************");
+                Map<String,Object> resultMap = searchResult.getDocument(Map.class);
+                for ( Iterator<String> iterator = resultMap.keySet().iterator(); iterator.hasNext(); ) {
+                    String key = iterator.next();
+                    System.out.println(String.format("%s : %s", key , resultMap.get(key)));
+                }
+            });
 
         } catch (Exception e) {
             System.err.println("Exception:" + e.getMessage());
